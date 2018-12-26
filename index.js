ec258eeba903f44aea771608e0358e4d77956f6e
Stephanie Tsuei <stsuei@buffalo.edu>
2018-12-26 20:51:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function takeANumber(lineOfPeople, name){
+  lineOfPeople.push(name)
+  return `Welcome, ${name}. You are number ${lineOfPeople.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 0){
+    var firstP = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${firstP}.`
+  }
+  else{
+      return "There is nobody waiting to be served!";
+    }
+}
+
+function currentLine(currentLine){
+  var j = 0;
+  var newArray = [];
+  if(currentLine.length > 0){
+    newArray.push(`${j+1}. ${currentLine[j]}`);
+    for(j = 1; j < currentLine.length; j++){
+      newArray.push(` ${j+1}. ${currentLine[j]}`);
+    }
+    newArray.join(' ');
+      return "The line is currently: " + newArray;
+  }else{
+    return 'The line is currently empty.'
+  }
+}
